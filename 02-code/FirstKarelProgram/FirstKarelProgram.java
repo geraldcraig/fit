b0985c4b5cdefada1edc9fefb86f020478b4a9bf
geraldcraig <geraldcraig@me.com>
2022-07-18 11:23:30 +0100

    karel added

---
+++ 02-code/FirstKarelProgram/FirstKarelProgram.java
@@ -0,0 +1,29 @@
+/*
+ * File: FirstKarelProgram.java 
+ * ----------------------------
+ * This program solves the problem of moving a beeper to a ledge.
+ */
+
+import stanford.karel.*;
+
+
+public class FirstKarelProgram extends Karel {
+	
+	
+	
+	public void run() {
+		move();
+		pickBeeper();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		putBeeper();
+		move();
+	}
+
+		
+}
