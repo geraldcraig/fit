9c10b7cea5872983178d21f5b62ed58e03aa3e81
Gerald Craig <geraldcraig@me.com>
2022-07-21 21:51:24 +0100

    thursday updated

--- Fit Week 01/src/thursday/ArrayExample16.java
+++ Fit Week 01/src/thursday/ArrayExample16.java
@@ -1,26 +1,66 @@
 package thursday;
 
+import java.util.Arrays;
+
 public class ArrayExample16 {
-	
-	static int[] add_sub(int val1, int val2) {
-		
-		int[] result = new int[2];
-		
-		result[0] = val1 + val2;
-		
-		result[1] = val1 - val2;
-		
-		return result;
-	}
 
 	public static void main(String[] args) {
 		
-		int[] result = add_sub(10, 5);
+		final int length = 5;
 		
-		System.out.println("val1 + val2 = " + result[0]);
+		Generic_Array<Integer>intArray = new Generic_Array(length);
 		
-		System.out.println("val1 - val2 = " + result[1]);
+		for (int i = 0; i < length; i++)
+			
+			intArray.set(i, i * 2);
 		
+		System.out.println("Integer Array elements:" + intArray);
+		
+		Generic_Array<String>strArray = new Generic_Array(length);
+		
+		for (int i = 0; i < length; i++)
+			
+			strArray.set(i, String.valueOf((char)(i + 97)));
+			
+			System.out.println("String Array Elements: " + strArray);
+		
+	}
+	
+	class Generic_Array<E> {
+		
+		private final Object[] gen_Array;
+		
+		public final int length;
+		
+		public Generic_Array(int length) {
+			
+			gen_Array = new Object[length];
+			
+			this.length = length;
+			
+		}
+		
+		Eget(int i) {
+			
+			final E e = (E)gen_Array[i];
+			
+			return e;
+					
+					
+		}
+		
+		void set(int i, E e) {
+			
+			gen_Array[i] = e;
+			
+		}
+		
+		@Override
+		
+		public String toString() {
+			
+			return Arrays.toString(gen_Array);
+		}
 	}
 	
 }
--- Fit Week 01/src/thursday/ArrayExample17.java
+++ Fit Week 01/src/thursday/ArrayExample17.java
@@ -1,25 +1,35 @@
 package thursday;
 
 public class ArrayExample17 {
-	
-	static int[] add_sub(int val1, int val2) {
-		
-		int[] result = new int[2];
-		
-		result[0] = val1 + val2;
-		
-		result[1] = val1 - val2;
-		
-		return result;
-	}
 
 	public static void main(String[] args) {
 		
-		int[] result = add_sub(10, 5);
+		int[] jagged_array[][]= new int[2][];
 		
-		System.out.println("val1 + val2 = " + result[0]);
+		jagged_array[0] = new int[4];
 		
-		System.out.println("val1 - val2 = " + result[1]);
+		jagged_array[1] = new int[2];
+		
+		for (int i = 0; i < jagged_array.length; i++)
+			
+			for (int j = 0; j < jagged_array[i].length; j++)
+				
+				jagged_array[i][j] = j + 1;
+		
+		System.out.println("Two-dimensional Jagged Array:");
+		
+		for (int i = 0; i < jagged_array.length; i++) {
+			
+			System.out.println("jagged_array[" + i + "]  ");
+			
+			for (int j = 0; j < jagged_array[i].length; j++)
+				
+				System.out.print(jagged_array[i][j] + " ");
+			
+			System.out.println();
+		}
+		
+		
 		
 	}
 	
