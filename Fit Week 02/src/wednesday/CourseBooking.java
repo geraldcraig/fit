11914c0e76ebf484a3d84b5d9ce22dbec4ab7309
geraldcraig <geraldcraig@me.com>
2022-07-28 15:25:36 +0100

    updated

--- Fit Week 02/src/wednesday/CourseBooking.java
+++ Fit Week 02/src/wednesday/CourseBooking.java
@@ -1,10 +1,36 @@
 package wednesday;
 
 public class CourseBooking {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String course_name;
+		DateType date;
+		double course_cost;
+		int course_availability;
 
+		
+		newFile();
+		existingFile();
+		bookingSeatPlan();
+		
+		
+	}
+
+	private static void bookingSeatPlan() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void existingFile() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void newFile() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
